345629e26e58200fc5387508aad6438ff8b1b39f
Minchan Kim <minchan@google.com>
2019-03-27 13:33:51 +0900

    Revert "zram: implement deduplication in zram"
    This reverts commit b96fb270ceadcc7afb0935b6334378eebf03404a.
    
    Bug: 129372836
    Change-Id: I32c6bd4b0ab7fa9a4896c2d7e92057305b200e3a
    Signed-off-by: Minchan Kim <minchan@google.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -19,10 +19,8 @@
 #include <linux/zsmalloc.h>
 #include <linux/crypto.h>
 #include <linux/mm.h>
-#include <linux/spinlock.h>
 
 #include "zcomp.h"
-#include "zram_dedup.h"
 
 #define SECTOR_SHIFT		9
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
@@ -66,10 +64,6 @@ enum zram_pageflags {
 /*-- Data structures */
 
 struct zram_entry {
-	struct rb_node rb_node;
-	u32 len;
-	u32 checksum;
-	unsigned long refcount;
 	unsigned long handle;
 };
 
@@ -101,11 +95,6 @@ struct zram_stats {
 	atomic64_t pages_stored;	/* no. of pages currently stored */
 	atomic_long_t max_used_pages;	/* no. of maximum pages stored */
 	atomic64_t writestall;		/* no. of write slow paths */
-	atomic64_t dup_data_size;	/*
-					 * compressed size of pages
-					 * duplicated
-					 */
-	atomic64_t meta_data_size;	/* size of zram_entries */
 	atomic64_t miss_free;		/* no. of missed free */
 #ifdef	CONFIG_ZRAM_WRITEBACK
 	atomic64_t bd_count;		/* no. of pages in backing device */
@@ -171,18 +160,11 @@ void swap_add_to_list(struct list_head *, swp_entry_t);
 void swap_writeback_list(struct zwbs **, struct list_head *);
 #endif
 
-struct zram_hash {
-	spinlock_t lock;
-	struct rb_root rb_root;
-};
-
 struct zram {
 	struct zram_table_entry *table;
 	struct zs_pool *mem_pool;
 	struct zcomp *comp;
 	struct gendisk *disk;
-	struct zram_hash *hash;
-	size_t hash_size;
 	/* Prevent concurrent execution of device init */
 	struct rw_semaphore init_lock;
 	/*
@@ -229,6 +211,4 @@ struct zram {
 	struct mutex blk_bitmap_lock;
 #endif
 };
-
-void zram_entry_free(struct zram *zram, struct zram_entry *entry);
 #endif
