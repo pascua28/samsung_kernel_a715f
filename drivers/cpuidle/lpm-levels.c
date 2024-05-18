9d7cbfef0ad6d6e022713f751ba24f7433a2bb76
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-05-18 23:55:25 +0800

    cpuidle: fix build without SEC_PM_DEBUG
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -764,10 +764,12 @@ static int lpm_suspend_prepare(void)
 
 #ifdef CONFIG_SEC_PM
 	regulator_showall_enabled();
+#ifdef CONFIG_SEC_PM_DEBUG
 	sec_clock_debug_print_enabled();
 
 	debug_masterstats_show("entry");
 	debug_rpmstats_show("entry");
+#endif
 #endif
 
 #ifdef CONFIG_SEC_PM_DEBUG
@@ -787,7 +789,7 @@ static void lpm_suspend_wake(void)
 	suspend_in_progress = false;
 	lpm_stats_suspend_exit();
 
-#ifdef CONFIG_SEC_PM
+#ifdef CONFIG_SEC_PM_DEBUG
 	sec_debug_print_sleep_time();
 	debug_rpmstats_show("exit");
 	debug_masterstats_show("exit");
