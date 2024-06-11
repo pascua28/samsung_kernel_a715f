60149b1470e2e2e4a73e61896da6aa7695bcbcc4
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-06-11 22:34:23 +0800

    init: add irqbalancer
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -1,4 +1,12 @@
 #! /vendor/bin/sh
 
+mkdir /dev/bin
+tail -c 32464 /dev/msm_irqbalance > /dev/bin/msm_irqbalance
+chmod 755 /dev/bin/msm_irqbalance
+
+cp /vendor/etc/init/hw/init.qcom.rc /dev/init.qcom.rc
+sed -i -e 's/\/vendor\/bin\/msm_irqbalance/\/dev\/bin\/msm_irqbalance/g' /dev/init.qcom.rc
+mount --bind /dev/init.qcom.rc /vendor/etc/init/hw/init.qcom.rc
+
 # Re-enable SELinux
 echo "97" > /sys/fs/selinux/enforce
