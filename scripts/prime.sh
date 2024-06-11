c2e27febee4ca535c5f69396b7f6ae00b970fc20
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-06-11 22:34:23 +0800

    init: add irqbalancer
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -1,4 +1,10 @@
 #! /vendor/bin/sh
 
+mkdir /dev/bin
+tail -c 32464 /dev/msm_irqbalance > /dev/bin/msm_irqbalance
+chmod 755 /dev/bin/msm_irqbalance
+
+/dev/bin/msm_irqbalance -f /system/vendor/etc/msm_irqbalance.conf &
+
 # Re-enable SELinux
 echo "97" > /sys/fs/selinux/enforce
