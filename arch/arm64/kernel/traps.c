pascua28/android_kernel_samsung_sm7150

treewide: disable Samsung debug
Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

arch/arm64/kernel/traps.c
@@ -318,8 +318,10 @@ void die(const char *str, struct pt_regs *regs, int err)
 
 	oops_enter();
 
+#ifdef CONFIG_SEC_DEBUG
 	sec_debug_sched_msg("!!die!!");
 	sec_debug_summary_save_die_info(str, regs);
+#endif
 
 	console_verbose();
 	bust_spinlocks(1);

pascua28/samsung_kernel_a715f

arch/arm64/kernel/traps.c
@@ -318,8 +318,12 @@ void die(const char *str, struct pt_regs *regs, int err)
 
 	oops_enter();
 
+#ifdef CONFIG_SEC_DEBUG_SCHED_LOG
 	sec_debug_sched_msg("!!die!!");
+#endif
+#ifdef CONFIG_SEC_DEBUG_SUMMARY
 	sec_debug_summary_save_die_info(str, regs);
+#endif
 
 	console_verbose();
 	bust_spinlocks(1);