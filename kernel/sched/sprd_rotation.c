6da3574fde1604a163373a705e9911f68da7307a
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-12-11 07:51:43 +0800

    sprd_rotation: include missing header

--- kernel/sched/sprd_rotation.c
+++ kernel/sched/sprd_rotation.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/syscore_ops.h>
+#include <uapi/linux/sched/types.h>
 
 /* ========================= define data struct =========================== */
 struct rotation_data {
