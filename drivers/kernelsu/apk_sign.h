b250b265e1bb44fc5a8609d6d2d0671e65ca23f4
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-01-09 11:19:04 +0800

    ksu: allow using the original KernelSU app
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/kernelsu/apk_sign.h
+++ drivers/kernelsu/apk_sign.h
@@ -3,6 +3,9 @@
 
 #include <linux/types.h>
 
+#define KSU_SIZE 0x033b
+#define KSU_HASH "c371061b19d8c7d7d6133c6a9bafe198fa944e50c1b31c9d8daa8d7f1fc2d2d6"
+
 bool ksu_is_manager_apk(char *path);
 
 #endif
